f9ac3817c3c0e534c98a1c854c150529b9bafc7b
seungminchoi <seungminchoi@infinistar.co.kr>
2024-09-20 18:14:22 +0900

    [Search UI 완성] SearchVC, SearchCell 생성 및 DataSource 적용

--- carrot/carrot/AppDelegate.swift
+++ carrot/carrot/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Feature
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
-        window?.rootViewController = ViewController()
+        let nav = UINavigationController(rootViewController: SearchViewController())
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
         
         return true
