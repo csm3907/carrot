dbaaa1f670d5dd6c296fafeadb959133569f9dec
seungminchoi <seungminchoi@infinistar.co.kr>
2024-09-20 11:24:05 +0900

    [Clean Architecture] 아키텍쳐 설정

---
+++ carrot/Design/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Design",
+    platforms:  [.iOS(.v15)],
+    products: [
+        .library(
+            name: "Design",
+            targets: ["Design"]),
+    ],
+    dependencies: [
+        .package(path: "../Core"),
+    ],
+    targets: [
+        .target(
+            name: "Design",
+            dependencies: [
+                .product(name: "Core", package: "Core"),
+            ]
+        ),
+    ]
+)
---
+++ carrot/Feature/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Feature",
+    platforms:  [.iOS(.v15)],
+    products: [
+        .library(
+            name: "Feature",
+            targets: ["Feature"]),
+    ],
+    dependencies: [
+        .package(path: "../Core"),
+        .package(path: "../Design"),
+        .package(path: "../Data"),
+        .package(path: "../Domain"),
+    ],
+    targets: [
+        .target(
+            name: "Feature",
+            dependencies: [
+                .product(name: "Core", package: "Core"),
+                .product(name: "Design", package: "Design"),
+                .product(name: "Domain", package: "Domain"),
+                .product(name: "Data", package: "Data"),
+            ]),
+    ]
+)
